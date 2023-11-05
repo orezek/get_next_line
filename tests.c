09ab87b04d8d7c40ee2edcd88661a11764f354a5
O.R <orezek@seznam.cz>
2023-11-05 10:41:12 +0100

    Test: update test file

--- tests.c
+++ tests.c
@@ -12,34 +12,43 @@
 void test_get_next_line() {
     // Create a temporary file
     char temp_filename[] = "/tmp/testfileXXXXXX";
+	char *temp_filename2 = "./file1_test.txt";
     int fd = mkstemp(temp_filename);
-    if (fd < 0) {
+	int fd2 = open(temp_filename2, O_RDONLY);
+    if (fd < 0 || fd2 < 0 ) {
         perror("Failed to open file");
         return;
 	}
 
-	// Write some lines to the file
-	//char *lines[] = {"1\n", "Line 2\n", "Line 3\n", "Line 444 444 sdfsdf44\n", "Line 5\n", "\n"};
-	char *lines[] = {"1jf;ladsjfl;asjfslfjdkfjdkfjdkfjffdfsfasdfalsjfs;lfjas;lfjsl;dfjsdlfjasfl;"};
-	//char *lines[] = {"Line 1\n", "Line 2\n", "Line 3\n", "Line 444 444 sdfsdf44\n", "Line 5\n", "\n", "", "   ", "\n\n\n\n\n"};
-	for (int i = 0; i < sizeof(lines) / sizeof(char*); i++) {
-		write(fd, lines[i], strlen(lines[i]));
-	}
+	// // Write some lines to the file
+	// //char *lines[] = {"1\n", "Line 2\n", "Line 3\n", "Line 444 444 sdfsdf44\n", "Line 5\n", "\n"};
+	// char *lines[] = {"1jf;ladsjfl;asjfslfjdkfjdkfjdkfjffdfsfasdfalsjfs;lfjas;lfjsl;dfjsdlfjasfl;"};
+	// //char *lines[] = {"Line 1\n", "Line 2\n", "Line 3\n", "Line 444 444 sdfsdf44\n", "Line 5\n", "\n", "", "   ", "\n\n\n\n\n"};
+	// for (int i = 0; i < sizeof(lines) / sizeof(char*); i++) {
+	// 	write(fd, lines[i], strlen(lines[i]));
+	// }
 
-	// Reset the file descriptor to the beginning of the file
-	lseek(fd, 0, SEEK_SET);
+	// // Reset the file descriptor to the beginning of the file
+	// lseek(fd, 0, SEEK_SET);
 
-	// Test get_next_line
-	for (int i = 0; i < sizeof(lines) / sizeof(char*); i++) {
-		char *line = get_next_line(fd);
+	// // Test get_next_line
+	// for (int i = 0; i < sizeof(lines) / sizeof(char*); i++) {
+	// 	char *line = get_next_line(fd);
+	// 	printf("%s", line);
+	// 	assert(line != NULL);
+	// 	assert(strcmp(line, lines[i]) == 0);
+	// 	free(line);
+	// }
+	char *line = get_next_line(fd2);
+	while(*line)
+	{
 		printf("%s", line);
-		assert(line != NULL);
-		assert(strcmp(line, lines[i]) == 0);
 		free(line);
+		line = get_next_line(fd2);
 	}
-
 	// Clean up
 	close(fd);
+	close(fd2);
 	remove(temp_filename);
 }
 
