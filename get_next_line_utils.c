0e3ca3d7a27aa2d83c5aa5032955a87afd2e747d
O.R <orezek@seznam.cz>
2023-11-06 00:38:47 +0100

    Update: add helper functions

---
+++ get_next_line_utils.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 22:24:23 by aldokezer         #+#    #+#             */
+/*   Updated: 2023/11/05 23:22:04 by aldokezer        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while(*str++)
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*new_str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	new_str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!new_str)
+		return (NULL);
+	while (s1[i])
+		new_str[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		new_str[j++] = s2[i++];
+	new_str[j] = '\0';
+	return (new_str);
+}
+
+int	ft_newline_position(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str != '\n')
+		i++;
+	return (i);
+}
+
+int	ft_has_newline(char *str)
+{
+	while (*str)
+	{
+		if (*str == '\n')
+			return (1);
+		str++;
+	}
+	return (0);
+}
