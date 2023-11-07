a0d94376f18bb8886a128a328fa557634279256a
O.R <orezek@seznam.cz>
2023-11-07 23:29:57 +0100

    Update: add function prototypes ft_extract and ft_has_newline

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 16:51:06 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/06 17:00:48 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/07 22:58:18 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@
 char	*get_next_line(int fd);
 int		ft_strlen(char *str);
 char	*ft_strjoin(char *s1, char *s2);
-int		ft_countbytes(char *str);
+char	*ft_extract_line_and_movebytes(char *buf);
+int		ft_has_newline(char *str);
 #endif
