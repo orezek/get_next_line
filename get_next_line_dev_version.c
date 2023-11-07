98e0f5e5dda29ccf1148916a91b42cf9714e6002
O.R <orezek@seznam.cz>
2023-11-07 23:49:25 +0100

    Edit: rename get_next_line_v2 to get_next_line_dev_version

--- get_next_line_dev_version.c
+++ get_next_line_dev_version.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_v2.c                                 :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 11:08:06 by orezek            #+#    #+#             */
-/*   Updated: 2023/11/07 23:35:26 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/07 18:39:17 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@
 // Todo: Add support for multiple file descriptors.
 
 // Steps:
-// 0. Caller calls get_next_line with a file descriptor and expects a line from the file or NULL.
+// 0. Caller calls get_next_line with a file descriptor and expects a line from the file.
 // 1. function entry - is buffer NULL?
 	// 1.1 if it is, allocate memory, read to buf, set trailing \0 to the buffer
 		// do all the checks including EOF and continue to 2.1 (if EOF return NULL)
@@ -48,9 +48,23 @@
 	//		// into a new_string var, shifts the buffer to the
 			// left and returns the new string var to the caller (continues to 0)
 	// 2.1.2 if it doesn't, it moves (strjoin)? the buffer into
-			// a new_line var, reads to the buffer, sets trailing \0 to the buffer
-			// and it contines until if either finds a new line or EOF (continues to 0)
-// 3. caller receives a NULL pointer and knows that there is no more data to read.
+			// a new_lie var, reads to the buffer, sets trailing \0 to the buffer
+			// and it contines until if either finds a new line or EOF
+// 3. check if buffer is not empty
+	// 3.1 if it is, free the buffer and return NULL to the caller
+	// 3.2 if it is not, return the buffer to the caller
+// 4. caller receives a NULL pointer and knows that there is no more data to read.
+int	ft_has_newline(char *str)
+{
+	while (*str)
+	{
+		if (*str == '\n')
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
 
 char	*get_next_line(int fd)
 {
@@ -59,43 +73,51 @@ char	*get_next_line(int fd)
 	char		*new_line;
 	char		*temp;
 	int			bytes_read;
-
 	buffer_size = 50;
-	new_line = NULL;
+
+// 1
 	if (buf == NULL)
 	{
 		buf = malloc(sizeof(char) * (buffer_size + 1));
 		bytes_read = read(fd, buf, buffer_size);
-		if (bytes_read <= 0)
-		{
-			free(buf);
-			return (buf = NULL);
-		}
 		buf[bytes_read] = '\0';
 	}
-	if (!ft_has_newline(buf))
+// 2.1.2
+	if (!ft_has_newline(buf)) // Do I need to check if buf is empty? It si checked automatically here.
 	{
-		while (!ft_has_newline(buf))
+		// new_line = strdup(buf); // check what happens when buf is empty
+		// bytes_read = read(fd, buf, buffer_size); // check EOF what happens when EOF is reached to buf
+		// buf[bytes_read] = '\0';
+		// if (ft_has_newline(buf))
+		// {
+		// 	temp = ft_extract_line_and_movebytes(buf);
+		// 	new_line = ft_strjoin(new_line, temp);
+		// 	free(temp);
+		// 	return (new_line);
+		// }
+		while (!ft_has_newline(buf) && bytes_read > 0)
 		{
-			new_line = ft_strjoin(new_line, buf);
+			new_line = ft_strjoin(new_line, buf); // does strjoin use malloc?
 			bytes_read = read(fd, buf, buffer_size);
-			if (bytes_read == 0)
-			{
-				free(buf);
-				buf = NULL;
-				return (new_line);
-			}
 			buf[bytes_read] = '\0';
+		}
+		if (bytes_read == 0)
+		{
+			free(buf);
+			return (new_line);
 		}
 		temp = ft_extract_line_and_movebytes(buf);
 		new_line = ft_strjoin(new_line, temp);
 		free(temp);
 		return (new_line);
 	}
+// 2.1.1 & 3.2 be careful if is true if there is content  this is error
 	if (ft_has_newline(buf))
 	{
 		new_line = ft_extract_line_and_movebytes(buf);
 		return (new_line);
 	}
+// 3.1
+	free(buf);
 	return (NULL);
 }
