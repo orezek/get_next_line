42186224a3981d759be99bc7b36690e185c37221
O.R <orezek@seznam.cz>
2023-11-08 21:00:16 +0100

    Update: implement custom ft_strncpy function

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 16:51:06 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/07 23:43:28 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/08 20:59:34 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,6 @@ int		ft_strlen(char *str);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_extract_line_and_movebytes(char *buf);
 int		ft_has_newline(char *str);
+int		initialize_buffer(int fd, char **buf);
+char	*ft_strncpy(char *dest, char *src, int n);
 #endif
